3b7109826ed21f0892a9bd4da7619379bf49986b
Maksim <121385213+nert1n@users.noreply.github.com>
2024-01-01 20:21:13 +0200

    Update style.js

--- src/js/style.js
+++ src/js/style.js
@@ -1,10 +1,4 @@
-// How to use
-// data-da="куда,когда,какой"
-import { useDynamicAdapt } from '/src/js/dynamicAdapt.js'
-useDynamicAdapt()
-
 document.querySelector('.burger').addEventListener('click', function () {
    document.querySelector('.burger').classList.toggle('active');
-   document.querySelector('.header__bar').classList.toggle('active');
-   document.querySelector('.header__button').classList.toggle('active');
+   document.querySelector('.nav__bar').classList.toggle('active');
 })
